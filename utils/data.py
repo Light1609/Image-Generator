f7e89269000d742e55043b2c498e80dceaf7df14
Light1609 <kelseiir001@gmail.com>
2024-06-17 11:53:24 +0000

    Initial commit

--- utils/data.py
+++ utils/data.py
@@ -2,26 +2,24 @@ import torch
 import torchvision.transforms as transforms
 import torchvision.datasets as dset
 
-
 def create_data_loader(args):
-	dataset = dset.ImageFolder(root=args.dataroot,
-		transform=transforms.Compose(
-			[
-			transforms.Resize(args.image_size),
-			transforms.CenterCrop(args.image_size),
-			transforms.ToTensor(),
-			transforms.Normalize(
-				(0.5,0.5,0.5),
-				(0.5, 0.5,0.5)
-				)
-			]))
+    # Asegúrate de que el tamaño de la imagen sea 16x16 o 32x32
+    if args.image_size not in [16, 32]:
+        raise ValueError("El tamaño de la imagen debe ser 16x16 o 32x32")
 
-	# setting up data loader
-	dataloader = torch.utils.data.DataLoader(dataset, 
-		batch_size=args.batch_size,
-		shuffle=True,
-		num_workers=args.workers
-		)
+    dataset = dset.ImageFolder(root=args.dataroot,
+        transform=transforms.Compose([
+            transforms.Resize(args.image_size),  # Usa el tamaño de imagen especificado en args.image_size
+            transforms.CenterCrop(args.image_size),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        ]))
 
-	return dataloader
+    # Configuración del DataLoader
+    dataloader = torch.utils.data.DataLoader(dataset, 
+        batch_size=args.batch_size,
+        shuffle=True,
+        num_workers=args.workers
+    )
 
+    return dataloader
